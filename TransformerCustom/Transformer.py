6a9363b0e299b139b06c3d68ba05d852c7a18a94
Mohamed Mohamed Ibrahim <mohamedgawad600@gmail.com>
2025-12-09 06:53:12 +0200

    Adding Transformer class.

---
+++ TransformerCustom/Transformer.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+
+from TransformerCustom.Decoder import Decoder
+from TransformerCustom.Encoder import Encoder
+
+class Transformer(nn.Module):
+    def __init__(
+            self,
+            src_vocab_size,
+            trg_vocab_size,
+            src_pad_idx,
+            trg_pad_idx,
+            embed_size=256,
+            num_layers=6,
+            forward_expansion=4,
+            heads=8,
+            dropout=0,
+            device="cpu",
+            max_length=100,
+    ):
+        super(Transformer, self).__init__()
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.device = device
+
+        self.encoder = Encoder(
+            src_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            device,
+            forward_expansion,
+            dropout,
+            max_length
+        )
+
+        self.decoder = Decoder(
+            trg_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            device,
+            forward_expansion,
+            dropout,
+            max_length
+        )
+
+    def make_src_mask(self, src):
+        return (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2).to(self.device)
+
+    def make_trg_mask(self, trg):
+        N, trg_len = trg.size()
+        return torch.tril(torch.ones(trg_len, trg_len)).expand(
+            N, 1, trg_len, trg_len
+        )
+
+    def forward(self, src, trg):
+        src_mask = self.make_src_mask(src)
+        trg_mask = self.make_trg_mask(trg)
+        enc_out = self.encoder(src, src_mask)
+        dec_out = self.decoder(trg, enc_out, src_mask, trg_mask)
+        return dec_out
+
